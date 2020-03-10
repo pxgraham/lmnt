be540a9058e0af8ae06e1afcb27d61f74b275ff6
unknown <pxgraham@gmail.com>
2020-03-09 20:15:01 -0700

    beginning of drawings

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,13 @@
 const socket = io({transports: ['websocket'], upgrade: false});
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const drawBoundaries = () => {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 850, canvas.width, 5);
+    ctx.fillRect(0, 575, 400, 5);
+    ctx.fillRect(2000-400, 575, 400, 5);
+    ctx.fillRect(550, 250, 900, 5)
+}
+
+drawBoundaries();
