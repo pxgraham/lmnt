9119c117e03839f6faeee89f7ca67a8cc217334e
unknown <pxgraham@gmail.com>
2020-03-09 19:46:27 -0700

    removed 404

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,7 @@
 const path = require("path");
 
 module.exports = (app) => {
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/pages/index.html"));
-    });
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/pages/404.html"));
+        res.sendFile(path.join(__dirname, "../public/pages/index.html"));
     });
 };
