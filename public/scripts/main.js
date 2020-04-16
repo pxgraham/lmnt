6ae6fbaeca1fb3187f4efdfaf3ac64f762d085c7
unknown <pxgraham@gmail.com>
2020-04-16 09:20:39 -0700

    changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,89 @@
 const socket = io({transports: ['websocket'], upgrade: false});
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const view = document.getElementById('view');
+const vctx = view.getContext('2d');
+let viewX = -875;
+let viewY = -750;
+let left = false;
+let right = false;
+const join = (color) => {
+    socket.emit('join', color);
+    setInterval(() => {
+        if(left) {viewX+=10}
+        if(right) {viewX-=10}
+        vctx.drawImage(canvas, viewX, viewY);
+    }, 1000/60)
+}
+let enemies = [];
+let map = {};
+socket.on('update', (data) => {
+    draw();
+    ctx.fillStyle = 'lime';
+    ctx.fillRect(1000, 800, 10, 100)
+    for(let i = 0; i < data.length; i++) {
+        let player = data[i];        
+        ctx.fillStyle = player.c;
+        ctx.fillRect(player.x, player.y, player.w, player.h);
+    }
+})
 
-const drawBoundaries = () => {
+const draw = () => {
+    vctx.drawImage(canvas, viewX, viewY);
+    //clear bkgnd
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    //boundaires
     ctx.fillStyle = 'red';
     ctx.fillRect(0, 850, canvas.width, 5);
-    ctx.fillRect(0, 575, 400, 5);
-    ctx.fillRect(2000-400, 575, 400, 5);
-    ctx.fillRect(550, 250, 900, 5)
+    ctx.fillRect(0, 50, canvas.width, 5);
+
+    //paint map
+    if(left) {viewX++}
+    if(right) {viewX--}
 }
 
-drawBoundaries();
+document.addEventListener('keyup', (e) => {
+    console.log(e.keyCode);
+    switch(e.keyCode) {
+        case 38:
+            // socket.emit('')
+            break;
+        case 40:
+            // dir.down = true;
+            break;
+        case 37:
+            left = false;
+            socket.emit('move', {input: 'left', state: false});
+            break;
+        case 39:
+            right = false;
+            socket.emit('move', {input: 'right', state: false});
+            break;
+        default: //do nothing
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.keyCode);
+    switch(e.keyCode) {
+        case 38:
+            // dir.up = true;
+            break;
+        case 40:
+            // dir.down = true;
+            break;
+        case 37:
+            left = true;
+            socket.emit('move', {input: 'left', state: true});
+            break;
+        case 39:
+            right = true;
+            socket.emit('move', {input: 'right', state: true});
+            break;
+        default: //do nothing
+    }
+})
+
+
--- server.js
+++ server.js
@@ -16,10 +16,67 @@ serv.listen(PORT, () => {
 
 const io = require('socket.io')(serv, {});
 
+let sessions = [];
+let players = [];
 
-io.sockets.on('connection', (socket) => { 
+function Player(x, y, w, h, id, viewX, viewY) {
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.id = id;
+  this.viewX = viewX;
+  this.viewY = viewY;
+  this.left = false;
+  this.right = false;
+  this.speed = 15;
+  this.move = () => {
+    if(this.left) {
+      this.x -= this.speed;
+      this.viewX += this.speed;
+    }
+    if(this.right) {
+      this.x += this.speed;
+      this.viewX -= this.speed;
+    }
+  }
+}
+
+io.sockets.on('connection', (socket) => {
+  sessions[socket.id] = socket;
+
+  socket.on('join', (playerData) => {
+    //need to set attributes
+    players[socket.id] = new Player(1000, 800, 50, 50, socket.id, -875, -750);
+    let player = players[socket.id];
+    player.c = playerData;
+  })
+
+  socket.on('move', (data) => {
+    if(players[socket.id]) {
+      switch(data.input) {
+        case 'left': players[socket.id].left = data.state;
+        break;
+        case 'right': players[socket.id].right = data.state;
+        break
+      }
+    }
+  })
+
   socket.on('disconnect', () => {
-    // nothing yet
+    delete sessions[socket.id];
+    delete players[socket.id];
   })
 
 })
+
+setInterval(() => {
+  let pack = [];
+  for(let i in players) {
+    players[i].move();
+    pack.push(players[i])
+  }
+  for(let i in sessions) {
+    sessions[i].emit('update', pack);
+  }
+},1000/60)
